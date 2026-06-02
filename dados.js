dc12d9dcb82c09cf028ea50d82bb0a8736daa6db
rfsignorini-spec <rfsignorini@gmail.com>
2026-06-02 16:29:07 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,10 @@
+const baseDRE = [
+    { linha: "Receita Bruta", nivel: 1, mes: 5, real: 1282, orc: 1400, aa: 1200 },
+    { linha: "Receita Líquida", nivel: 1, mes: 5, real: 1158, orc: 1290, aa: 1090 },
+    { linha: "Custo Variável", nivel: 1, mes: 5, real: -980, orc: -900, aa: -900 },
+    { linha: "Lucro Bruto", nivel: 1, mes: 5, real: 178, orc: 390, aa: 190 },
+    { linha: "Despesas Variáveis", nivel: 1, mes: 5, real: -46, orc: -45, aa: -40 },
+    { linha: "Comissões", nivel: 2, mes: 5, real: -32, orc: -30, aa: -28 },
+    { linha: "EBITDA", nivel: 1, mes: 5, real: 148, orc: 140, aa: 90 },
+    { linha: "Lucro Líquido", nivel: 1, mes: 5, real: 120, orc: 113, aa: 70 }
+];
