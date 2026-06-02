8098ab14bd5a290cd5656da13d35c0903ae759b2
rfsignorini-spec <rfsignorini@gmail.com>
2026-06-02 16:43:53 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,10 +1,8 @@
-const baseDRE = [
-    { linha: "Receita Bruta", nivel: 1, mes: 5, real: 1282, orc: 1400, aa: 1200 },
-    { linha: "Receita Líquida", nivel: 1, mes: 5, real: 1158, orc: 1290, aa: 1090 },
-    { linha: "Custo Variável", nivel: 1, mes: 5, real: -980, orc: -900, aa: -900 },
-    { linha: "Lucro Bruto", nivel: 1, mes: 5, real: 178, orc: 390, aa: 190 },
-    { linha: "Despesas Variáveis", nivel: 1, mes: 5, real: -46, orc: -45, aa: -40 },
-    { linha: "Comissões", nivel: 2, mes: 5, real: -32, orc: -30, aa: -28 },
-    { linha: "EBITDA", nivel: 1, mes: 5, real: 148, orc: 140, aa: 90 },
-    { linha: "Lucro Líquido", nivel: 1, mes: 5, real: 120, orc: 113, aa: 70 }
+const DRE = [
+{linha:"Receita Líquida", nivel:1, mes:5, real:1158, orc:1290, acumR:6550, acumO:6860},
+{linha:"Custo", nivel:1, mes:5, real:-980, orc:-900, acumR:-5300, acumO:-5200},
+{linha:"Lucro Bruto", nivel:1, mes:5, real:178, orc:390, acumR:1250, acumO:1660},
+{linha:"Despesa", nivel:1, mes:5, real:-46, orc:-45, acumR:-260, acumO:-255},
+{linha:"EBITDA", nivel:1, mes:5, real:148, orc:140, acumR:720, acumO:700},
+{linha:"Lucro Líquido", nivel:1, mes:5, real:120, orc:113, acumR:600, acumO:560}
 ];
